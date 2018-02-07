5327aca828a88dac501ece44d88ec55048f09d7c
Adam Kalt <adam.kalt.1@gmail.com>
2018-02-07 05:01:15 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,29 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (let i = 0; i < musicians.length; i++) {
+     var x = musicians[i]
+     var y = instruments[i]
+     var z = x + " plays " + y
+     array.push (z)
+  }
+
+return array;
+}
+
+function johnLennonFacts(facts) {
+  var array = [];
+let i = 0;
+ while (i < facts.length){
+   array.push (facts[i] + "!!!");
+   i++;
+  }
+return array;
+}
+
+function iLoveTheBeatles(number) {
+  var array = [];
+  do array.push ("I love the Beatles!");
+ while (number < 15 && maybeTrue());
+return array;
+}
+
