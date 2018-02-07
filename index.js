300058be38673eee32ffacf30393bd05c31ad206
Adam Kalt <adam.kalt.1@gmail.com>
2018-02-07 05:28:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,8 +22,7 @@ return array;
 
 function iLoveTheBeatles(number) {
   var array = [];
-  do {array.push ("I love the Beatles!");  
-  console.log (number++)}
+  do {array.push ("I love the Beatles!"); number++}
  while (number < 15);
 return array;
 }
