fa75f3ea7857e148b59b5c333e3bdbb1a413d64b
Adam Kalt <adam.kalt.1@gmail.com>
2018-02-07 05:10:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ return array;
 function iLoveTheBeatles(number) {
   var array = [];
   do {array.push ("I love the Beatles!");}
+  let number = 0
  while (number < 15 && maybeTrue());
  function maybeTrue() {
   return Math.random() >= 15;}
